ac3c53382514f0a79ab39e1d3a9096c71a5a538a
mahen <mahen@PRASHANTH>
2023-09-12 09:03:05 +0530

    Add project files.

---
+++ Drag&DropUI/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Drag_DropUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtSEmail = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtSMPT = new System.Windows.Forms.TextBox();
+            this.txtREmail = new System.Windows.Forms.TextBox();
+            this.txtSub = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(289, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EMAIL DETAILS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(102, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sender Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(102, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(269, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sender Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(102, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SMPT Server";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(102, 322);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 37);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Server Port";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(102, 417);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(237, 37);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Recipient Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(102, 492);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 37);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Subject";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(102, 584);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(174, 37);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "BrowseFile";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(716, 140);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 37);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Body";
+            // 
+            // txtSEmail
+            // 
+            this.txtSEmail.Location = new System.Drawing.Point(379, 135);
+            this.txtSEmail.Name = "txtSEmail";
+            this.txtSEmail.Size = new System.Drawing.Size(270, 26);
+            this.txtSEmail.TabIndex = 9;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(379, 195);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(270, 26);
+            this.txtPwd.TabIndex = 10;
+            // 
+            // txtSMPT
+            // 
+            this.txtSMPT.Location = new System.Drawing.Point(379, 260);
+            this.txtSMPT.Name = "txtSMPT";
+            this.txtSMPT.Size = new System.Drawing.Size(270, 26);
+            this.txtSMPT.TabIndex = 11;
+            // 
+            // txtREmail
+            // 
+            this.txtREmail.Location = new System.Drawing.Point(379, 427);
+            this.txtREmail.Name = "txtREmail";
+            this.txtREmail.Size = new System.Drawing.Size(270, 26);
+            this.txtREmail.TabIndex = 12;
+            // 
+            // txtSub
+            // 
+            this.txtSub.Location = new System.Drawing.Point(379, 492);
+            this.txtSub.Name = "txtSub";
+            this.txtSub.Size = new System.Drawing.Size(270, 26);
+            this.txtSub.TabIndex = 13;
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(811, 140);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(364, 334);
+            this.txtBody.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(379, 371);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 36);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "SSL";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(379, 322);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(270, 26);
+            this.txtPort.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(387, 560);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 61);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(847, 505);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(328, 67);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Send Email";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 688);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtSub);
+            this.Controls.Add(this.txtREmail);
+            this.Controls.Add(this.txtSMPT);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtSEmail);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtSEmail;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtSMPT;
+        private System.Windows.Forms.TextBox txtREmail;
+        private System.Windows.Forms.TextBox txtSub;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Drag&DropUI/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drag_DropUI
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=Wings;Integrated Security=True");
+        OpenFileDialog attchment;
+        string filename = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Load_data()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Email",con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into Emails Values('"+txtSEmail.Text+ "','"+txtPwd.Text+ "','"+txtSMPT.Text+"','"+txtPort.Text+"','"+txtREmail.Text+"','"+txtSub.Text+"','"+txtBody.Text+"')", con);
+                cmd.ExecuteNonQuery();
+                Load_data();
+                con.Close();
+                SmtpClient C = new SmtpClient();
+                C.Port = Convert.ToInt32(txtPort.Text.Trim());
+                C.Host = txtSMPT.Text.Trim();
+                C.EnableSsl = checkBox1.Checked;
+                C.DeliveryMethod = SmtpDeliveryMethod.Network;
+                C.UseDefaultCredentials = false;
+                C.Credentials = new NetworkCredential(txtSEmail.Text.Trim(), txtPwd.Text.Trim());
+
+
+                MailMessage m = new MailMessage();
+                m.From = new MailAddress(txtSEmail.Text.Trim());
+                m.To.Add(txtREmail.Text.Trim());
+                m.Subject = txtSub.Text.Trim();
+                m.IsBodyHtml = checkBox1.Checked;
+                m.Body = txtBody.Text.Trim();
+               
+
+                if (filename.Length >= 0)
+                {
+                    Attachment at = new Attachment(filename);
+                    m.Attachments.Add(at);
+                }
+                C.Send(m);
+                filename = "";
+                MessageBox.Show("Email Sent");
+                
+               
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                attchment = new OpenFileDialog();
+                attchment.Filter = "Images(.jpg,.png)|*.png;*.jpg;|pdf Files|*.pdf";
+                if (attchment.ShowDialog() == DialogResult.OK)
+                {
+                    filename = attchment.FileName;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
